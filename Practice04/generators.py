af41a34bf1359abcc006f12275f27fc04b62ad02
zhadyrakamaladinova <kamaladinovaz@gmail.com>
2026-02-28 01:36:46 +0500

    Add files via upload

---
+++ Practice04/generators.py
@@ -0,0 +1,29 @@
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+    print(x)
+
+
+mystr = "banana"
+
+for x in mystr:
+  print(x)
